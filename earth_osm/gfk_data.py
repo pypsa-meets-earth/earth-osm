6672e368bdd5243f3d355120ab56630062e643f8
mnm-matin <matin.mahmood99@gmail.com>
2022-10-15 14:26:42 +0100

    add gfk_data header

---
+++ earth_osm/gfk_data.py
@@ -0,0 +1,10 @@
+__author__ = "PyPSA meets Earth"
+__copyright__ = "Copyright 2022, The PyPSA meets Earth Initiative"
+__license__ = "MIT"
+
+"""Geofabrik Data Config
+
+This module contains functions to set config for Geofabrik data.
+
+"""
+
