e896d4a1c9c8cb0def0d3834dfd1cf080136b66b
mnm-matin <matin.mahmood99@gmail.com>
2022-10-15 13:51:51 +0100

    gfk_download header

---
+++ earth_osm/gfk_download.py
@@ -0,0 +1,10 @@
+__author__ = "PyPSA meets Earth"
+__copyright__ = "Copyright 2022, The PyPSA meets Earth Initiative"
+__license__ = "MIT"
+
+"""Geofabrik Data Download
+
+This module contains functions to download Geofabrik data.
+
+"""
+
