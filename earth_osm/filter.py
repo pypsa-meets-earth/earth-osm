f99a8be0349e86ba49e724753ecb6da5af9b5d74
mnm-matin <matin.mahmood99@gmail.com>
2022-10-15 15:16:32 +0100

    primary dict filter

--- earth_osm/filter.py
+++ earth_osm/filter.py
@@ -57,3 +57,33 @@ def run_feature_filter(primary_dict, feature_name):
     
     return feature_dict
         
+def run_primary_filter(PBF_inputfile, primary_file, primary_name, multiprocess):
+    logger.info('New Pre-Filter Data')
+    logger.info('Load OSM data from '+ PBF_inputfile+'\n')
+
+    feature_list = list(primary_feature_element[primary_name].keys())
+    pre_filter = {
+        Node: {primary_name: feature_list},
+        Way: {primary_name: feature_list},
+        Relation: {primary_name: feature_list},
+    }
+
+    primary_data = filter_pbf(PBF_inputfile,pre_filter,multiprocess)
+
+    metadata = {
+        'filter_date': str(datetime.now().isoformat()),
+        'primary_feature': primary_name
+    }
+    primary_dict = {
+        'Metadata': metadata,
+        'Data': primary_data
+    }
+    # Save primary_dict
+    with open(primary_file, "w", encoding="utf-8") as target:
+        json.dump(
+            primary_dict, target, ensure_ascii=False, indent=4, separators=(",", ":")
+        )
+    
+    return primary_dict
+
+
