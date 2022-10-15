3fa2d95d7f7107d3f679fb6bb32fc57f47b16269
mnm-matin <matin.mahmood99@gmail.com>
2022-10-15 14:27:55 +0100

    download sitemap

--- earth_osm/gfk_data.py
+++ earth_osm/gfk_data.py
@@ -8,3 +8,19 @@ This module contains functions to set config for Geofabrik data.
 
 """
 
+
+import os
+import geopandas as gpd
+import pandas as pd
+import json
+from collections import namedtuple
+
+from earth_osm.gfk_download import download_sitemap
+
+import logging
+logger = logging.getLogger("osm_data_extractor")
+logger.setLevel(logging.DEBUG)
+
+
+pkg_data_dir = os.path.join(os.path.dirname(__file__), 'data')
+sitemap = download_sitemap(False, pkg_data_dir)
