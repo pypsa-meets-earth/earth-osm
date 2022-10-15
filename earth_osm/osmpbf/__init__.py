46f5195f27c3949a16373ae8030379aacdb4f007
mnm-matin <matin.mahmood99@gmail.com>
2022-10-15 14:59:58 +0100

    add Node namedtuple

--- earth_osm/osmpbf/__init__.py
+++ earth_osm/osmpbf/__init__.py
@@ -1,1 +1,12 @@
+from collections import namedtuple
+
+Node = namedtuple('Node', ('id', 'tags', 'lonlat'))
+"""
+A OpenStreetMap `node <https://wiki.openstreetmap.org/wiki/Node>
+
+id: int (node id)
+tags: dict (tag names -> tag values)
+lonlat: tuple (lon, lat)
+"""
+
 
