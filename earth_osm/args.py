dbaa83c358e492c898099846b3c8a99ff8cbacf5
mnm-matin <matin.mahmood99@gmail.com>
2022-10-15 15:26:52 +0100

    add basic argparser

--- earth_osm/args.py
+++ earth_osm/args.py
@@ -7,3 +7,22 @@ __license__ = "MIT"
 This module provides a CLI interface for the earth_osm project.
 
 """
+
+import argparse
+import os
+
+from earth_osm.gfk_data import get_all_valid_list, view_regions
+from earth_osm.eo import get_osm_data
+from earth_osm.config import primary_feature_element
+
+def main():  # pragma: no cover
+    """
+    The main function executes on commands:
+    `python -m earth_osm` and `$ earth_osm `. 
+    It parses the command line and executes the appropriate function.
+    """
+
+    parser = argparse.ArgumentParser(
+        description='Earth-OSM by PyPSA-meets-Earth',
+        # epilog='''Example:''',
+        add_help=False) # hide default help
