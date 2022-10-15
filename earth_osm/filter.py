2c4fdfa4b6e3681be56b8daeb5bfe04f390124bf
mnm-matin <matin.mahmood99@gmail.com>
2022-10-15 15:15:20 +0100

    add feature filter header

---
+++ earth_osm/filter.py
@@ -0,0 +1,10 @@
+__author__ = "PyPSA meets Earth"
+__copyright__ = "Copyright 2022, The PyPSA meets Earth Initiative"
+__license__ = "MIT"
+
+"""Filter the Extracted OSM Data
+
+This module filters the extracted OSM data.
+
+"""
+
