ef1052afa447784ac1e1606d145b22b096f8c922
mnm-matin <matin.mahmood99@gmail.com>
2022-10-15 15:18:47 +0100

    conv ways to points

--- earth_osm/utils.py
+++ earth_osm/utils.py
@@ -35,3 +35,41 @@ def lonlat_lookup(df_way, primary_data):
     lonlat_list = df_way["refs"].apply(look)
 
     return lonlat_list
+
+def convert_ways_points(df_way, primary_data):
+    """
+    Convert Ways to Point Coordinates
+    """
+    lonlat_list = lonlat_lookup(df_way, primary_data)
+    way_polygon = list(
+        map(
+            lambda lonlat: Polygon(lonlat) if len(lonlat) >= 3 else Point(lonlat[0]),
+            lonlat_list,
+        )
+    )
+    area_column = list(
+        map(
+            int,
+            round(
+                gpd.GeoSeries(way_polygon)
+                .set_crs("EPSG:4326")
+                .to_crs("EPSG:3857")
+                .area,
+                -1,
+            ),
+        )
+    )  # TODO: Rounding should be done in cleaning scripts
+
+    def find_center_point(p):
+        if p.geom_type == "Polygon":
+            center_point = p.centroid
+        else:
+            center_point = p
+        return list((center_point.x, center_point.y))
+
+    lonlat_column = list(map(find_center_point, way_polygon))
+
+    # df_way.drop("refs", axis=1, inplace=True, errors="ignore")
+    df_way.insert(0, "Area", area_column)
+    df_way.insert(0, "lonlat", lonlat_column)
+
