7db6b97ae27e6af9600dc9dd7abe26034635af2e
mnm-matin <matin.mahmood99@gmail.com>
2022-10-15 13:54:33 +0100

    add earth_downloader function

--- earth_osm/gfk_download.py
+++ earth_osm/gfk_download.py
@@ -8,3 +8,39 @@ This module contains functions to download Geofabrik data.
 
 """
 
+
+import os
+import logging
+from tqdm.auto import tqdm
+import requests
+import urllib3
+import shutil
+import hashlib
+
+
+logger = logging.getLogger("osm_geo")
+logger.setLevel(logging.INFO)
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def earth_downloader(url, dir):
+    filename = os.path.basename(url)
+    filepath = os.path.join(dir, filename)
+    logger.info(f"{filename} downloading to {filepath}")
+    os.makedirs(os.path.dirname(filepath), exist_ok=True) #  create download dir
+    with requests.get(url, stream=True, verify=False) as r:
+        if r.status_code == 200:
+            # url properly found, thus execute as expected
+            file_size = int(r.headers.get('Content-Length', 0))
+            desc = "(Unknown total file size)" if file_size == 0 else ""
+            with tqdm.wrapattr(r.raw, "read", total=file_size, desc=desc, leave=False) as raw:
+                with open(filepath, "wb") as f:
+                    shutil.copyfileobj(raw, f)
+        else:
+            # error status code: file not found
+            logger.error(
+                f"Error code: {r.status_code}. File {filename} not downloaded from {url}"
+            )
+            filepath = None
+    return filepath
+
