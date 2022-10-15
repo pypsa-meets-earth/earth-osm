a09f325dd70dd00d5b7f21927baea9db4ab0a682
mnm-matin <matin.mahmood99@gmail.com>
2022-10-15 15:00:25 +0100

    add way namedtuple

--- earth_osm/osmpbf/__init__.py
+++ earth_osm/osmpbf/__init__.py
@@ -9,4 +9,12 @@ tags: dict (tag names -> tag values)
 lonlat: tuple (lon, lat)
 """
 
+Way = namedtuple('Way', ('id', 'tags', 'refs'))
+"""
+A OpenStreetMap `way <https://wiki.openstreetmap.org/wiki/Way>
+
+id: int (way id)
+tags: dict (tag names -> tag values)
+refs: list (node ids)
+"""
 
