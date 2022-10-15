c444c26fccf981c7892967cd313c750e259ba935
mnm-matin <matin.mahmood99@gmail.com>
2022-10-15 15:15:53 +0100

    filter primary data

--- earth_osm/filter.py
+++ earth_osm/filter.py
@@ -8,3 +8,31 @@ This module filters the extracted OSM data.
 
 """
 
+from datetime import datetime
+from earth_osm.gfk_download import download_pbf
+from earth_osm.extract import filter_pbf
+from earth_osm.osmpbf import Node, Relation, Way
+from earth_osm.config import primary_feature_element
+
+import os
+import json
+import logging
+
+
+logging.basicConfig()
+logger=logging.getLogger(__name__)
+#logger.setLevel(logging.INFO)
+logger.setLevel(logging.WARNING)
+
+
+def feature_filter(primary_data, filter_tuple = ('power', 'line')):
+
+    if set(primary_data.keys()) != set(['Node', 'Way', 'Relation']):
+        logger.error('malformed primary_data')
+    
+    feature_data={'Node':{},'Way':{},'Relation':{}}
+    for element in list(feature_data.keys()):
+        for id in primary_data[element]:
+            if filter_tuple in primary_data[element][id]["tags"].items():
+                feature_data[element][id] = primary_data[element][id]
+    return feature_data
