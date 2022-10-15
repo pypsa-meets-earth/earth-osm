18dbb01cd936a4132f417d23e50a6281bace3c05
mnm-matin <matin.mahmood99@gmail.com>
2022-10-15 13:48:45 +0100

    add args entry point header

---
+++ earth_osm/args.py
@@ -0,0 +1,9 @@
+__author__ = "PyPSA meets Earth"
+__copyright__ = "Copyright 2022, The PyPSA meets Earth Initiative"
+__license__ = "MIT"
+
+"""CLI interface for earth_osm project.
+
+This module provides a CLI interface for the earth_osm project.
+
+"""
