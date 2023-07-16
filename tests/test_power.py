cb849bb452257023579379999eca65a0120429f0
Matin Mahmood <mmahmood@exseed.ed.ac.uk>
2023-07-16 18:22:06 +0100

    improve test

--- tests/test_power.py
+++ tests/test_power.py
@@ -1,65 +1,16 @@
 from earth_osm.eo import save_osm_data
 
 
-def test_tower():
-    save_osm_data(
-        region_list=["germany"],
-        primary_name="power",
-        feature_list=["tower"],
-        update=False,
-        mp=True,
-        out_format=["csv", "geojson"],
-        out_aggregate=False,
-    )
+primary_name = "power"
 
-def test_substation():
-    save_osm_data(
-        region_list=["germany"],
-        primary_name="power",
-        feature_list=["substation"],
-        update=False,
-        mp=True,
-        out_format=["csv", "geojson"],
-        out_aggregate=False,
-    )
+# test low resource region (benin)
+# test high resource region (germany)
 
-def test_generator():
-    save_osm_data(
-        region_list=["germany"],
-        primary_name="power",
-        feature_list=["generator"],
-        update=False,
-        mp=True,
-        out_format=["csv", "geojson"],
-        out_aggregate=False,
-    )
+# test low resource feature (cable)
+# test high resource feature (substation)
 
-def test_line():
-    save_osm_data(
-        region_list=["germany"],
-        primary_name="power",
-        feature_list=["line"],
-        update=False,
-        mp=True,
-        out_format=["csv", "geojson"],
-        out_aggregate=False,
-    )
-
-def test_empty_cable():
-    save_osm_data(
-        region_list=["benin"],
-        primary_name="power",
-        feature_list=["cable"],
-        update=False,
-        mp=True,
-        out_format=["csv", "geojson"],
-        out_aggregate=False,
-    )
-
-
-if __name__ == '__main__':
-    test_tower()
-    test_substation()
-    test_generator()
-    test_line()
-    test_empty_cable()
+# test aggregation by region (benin, germany)
+# test aggregation by feature (tower, line)
+# test no aggregation
+ 
+ 
