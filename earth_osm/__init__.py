7c44cc5cab450f75769431958c7b80489e53e9fe
Davide Fioriti <fioritidavidesubs@gmail.com>
2024-12-11 12:06:38 +0100

    Implement dynamic versioning pyproject

--- earth_osm/__init__.py
+++ earth_osm/__init__.py
@@ -2,3 +2,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO)  # Basic configuration
 logger = logging.getLogger('eo')
+
+# specify version 
+with open("VERSION") as f:
+    __version__ = f.read().strip()
